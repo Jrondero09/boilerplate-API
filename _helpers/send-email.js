fb1ac9f90c88261641bc28cdc2ae12571a26c3a8
Jayann Rondero <ronderojayann@gmail.com>
2024-10-01 14:27:35 +0800

    new

---
+++ _helpers/send-email.js
@@ -0,0 +1,9 @@
+const nodemailer = rquire('nodemailer');
+const config = require('config.json');
+
+module.exports = sendEmail;
+
+async function sendEmail({ to, subject, html, from = config.emailFrom }) {
+    const transporter = nodemailer.createTransport(config.smtpOptions);
+    await transporter.sendMail({ from, to, subject, html });
+}
